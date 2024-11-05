5677d1cbfc5078d6d0848c6b65e5506fb1a71a95
Mikhail2107 <mixa.proshin@yandex.ru>
2024-11-06 00:25:34 +0300

    Добавил анисацию, заголовок, стили заголовка, сетка, отцентровал меню

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,84 @@
-import {useRef, useLayoutEffect, useState } from 'react';
+import { useRef, useLayoutEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const circleRef = useRef<HTMLUListElement>(null); 
+  const circleRef = useRef<HTMLUListElement>(null);
   const [eventList, setEventList] = useState([
-    { id: 1, title: 'История', style: {}, styleButton: {} }, //История
-    { id: 2, title: '', style: {}, styleButton: {} }, //Наука
-    { id: 3, title: '', style: {}, styleButton: {} }, //Кино
-    { id: 4, title: '', style: {}, styleButton: {} },
-    { id: 5, title: '', style: {}, styleButton: {} }, //Литература
-    { id: 6, title: '', style: {}, styleButton: {} },
+    { id: 1, title: 'История', style: {}, styleButton: {} },
+    { id: 2, title: 'Наука', style: {}, styleButton: {} },
+    { id: 3, title: 'Кино', style: {}, styleButton: {} },
+    { id: 4, title: 'Музыка', style: {}, styleButton: {} },
+    { id: 5, title: 'Литература', style: {}, styleButton: {} },
+    { id: 6, title: 'Искусство', style: {}, styleButton: {} },
   ]);
-  const [menuRotate, setMenuRotate] = useState({style: {}})
+  const [menuRotate, setMenuRotate] = useState({ style: {} });
+  const [activeId, setActiveId] = useState(null); 
 
-  useLayoutEffect(() => { 
+  useLayoutEffect(() => {
     const circle = circleRef.current;
-    if (circle) { 
+    if (circle) {
       const sectorAngle = 360 / eventList.length;
 
       setEventList((prevEventList) =>
         prevEventList.map((event, index) => {
-          const angle = sectorAngle * index+75;
-          console.log(angle+' '+index)
+          const angle = sectorAngle * index + 75;
           return {
             ...event,
             style: {
-              transform: ` rotate(${angle}deg)`,
+              transform: `rotate(${angle}deg)`,
               transformOrigin: '100% 100%',
             },
-            styleButton: {transform: ` rotate(${-75}deg)`,}
+            styleButton: { transform: `rotate(${-75}deg)` },
           };
         })
       );
     }
-  }, []); 
+  }, []);
 
   const handleRotateMenu = (id: number) => {
-    
-    const deg = (id-1) * 60
-    setMenuRotate({style: {transform: `rotate(-${deg}deg)`}})
-    console.log(`click ${deg}`, id)
-  }
+    const sectorAngle = 360 / eventList.length;
+    const deg = (id - 1) * sectorAngle;
+    setMenuRotate({ style: { transform: `rotate(-${deg}deg)` } });
+    setActiveId(id); 
+  };
 
   return (
     <>
+    <div className="history-container">
+      <div className="grid-container">
+        <div className="grid-item"></div>
+        <div className="grid-item"></div>
+        <div className="grid-item"></div>
+        <div className="grid-item"></div>
+      </div>
+      <h1 className='history-title'>Исторические даты</h1>
+      <div className="circle-box">
       <ul className="circle-menu" ref={circleRef} style={menuRotate.style}>
         {eventList.map((event) => (
           <li key={event.id} className="circle-item" style={event.style}>
-            <div className='circle-info'
-                style={event.styleButton}>            
-              <button className='buttonTitle'
+            <div className="circle-info">
+              <button
+                className={`buttonTitle ${activeId === event.id ? 'active' : ''}`}
                 onClick={() => handleRotateMenu(event.id)}
-                >
+                style={event.styleButton}
+              >
                 {event.id}
+                <span
+                  className={`circle-item-title ${
+                    activeId === event.id ? 'active' : ''
+                  }`}
+                >
+                  {event.title}
+                </span>
               </button>
             </div>
-              <span>{event.title}</span>
           </li>
         ))}
       </ul>
+      </div>
+        
+    </div>
+    
     </>
   );
 }
